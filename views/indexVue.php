475f4d15f0fca9cd2b058ccc052fcdc5728b0b77
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-11-29 17:16:29 +0100

    customized home page

--- views/indexVue.php
+++ views/indexVue.php
@@ -2,7 +2,34 @@
   include("template/header.php")
  ?>
 
-<p>This is the index view</p>
+<div class="container">
+  <a href="#" class="btn btn-custom">Add book</a>
+  <a href="#" class="btn btn-custom">List of users</a>
+
+
+  <h2 class="text-center">List of books</h2>
+  <div class="dropdown">
+    <button type="button" class="btn btn-custom btn-sm dropdown-toggle" data-toggle="dropdown">
+      Category
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Romance</a>
+      <a class="dropdown-item" href="#">Adventure</a>
+      <a class="dropdown-item" href="#">Thriller</a>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="card .col-sm-4 .col-md-4 .col-lg-4 .col-xl-4" style="width: 20rem;">
+      <div class="card-block">
+        <h4 class="card-title">Card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-custom">See details</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 
  <?php
    include("template/footer.php")
--- views/template/header.php
+++ views/template/header.php
@@ -3,15 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>Library Handle</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" >
     <link rel="stylesheet" href="../assets/css/main.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
   </head>
   <body>
     <header>
       <div class="header jumbotron">
           <a href="../controllers/index.php" class="btn btn-custom">HOME</a>
-          <h1 class="text-center">List of books</h1>
+          <h1 class="text-center">Libary handle</h1>
       </div>
 
     </header>
