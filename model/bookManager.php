72245447e2375e148d86c3d2210795382b64e1b9
Laure-Anne Urbaniak <laureane.urbaniak@gmail.com>
2017-11-29 19:39:00 +0100

    add bookManager and book object

---
+++ model/bookManager.php
@@ -0,0 +1,26 @@
+<?php
+
+class BookManager {
+  private $db;
+
+  public function __construct($db){
+    $this->setDb($db);
+  }
+
+  public function getDb()
+  {
+      return $this->db;
+  }
+
+  /**
+   * @param mixed $db
+   */
+  public function setDb($db)
+  {
+      $this->db = $db;
+  }
+}
+
+
+
+ ?>
